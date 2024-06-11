a02490d28fa89ef9bc613a25421c081c177b8e9c
aryan <ar4179@columbia.edu>
2024-06-11 00:39:13 -0500

    added Collapsible component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css';
-
-function App() {
+import Collapsible from "./Collapsible";
+import "./assets/index.css";
+import "./assets/icons.css";
+const App: React.FC = () => {
   return (
-    <div>
-
-    </div>
+    <Collapsible open header="collapse header">
+      Consectetur adipiscing elit pellentesque habitant morbi tristique.
+      Pulvinar pellentesque habitant morbi tristique. Vel quam elementum
+      pulvinar etiam. Pulvinar pellentesque habitant morbi tristique senectus
+    </Collapsible>
   );
-}
-
+};
 export default App;
+
