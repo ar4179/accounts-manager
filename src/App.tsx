2b3b86470320837b94c25426c047c9a1e14bfb74
aryan <ar4179@columbia.edu>
2024-06-11 00:47:44 -0500

    fixed multiple collapsible components issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import './App.css';
 import Collapsible from "./Collapsible";
+import React from "react";
 import "./assets/index.css";
 import "./assets/icons.css";
-const App: React.FC = () => {
+
+function App() {
   return (
-    <Collapsible open header="collapse header">
+    <div>
+    <Collapsible header="collapse header">
       Consectetur adipiscing elit pellentesque habitant morbi tristique.
       Pulvinar pellentesque habitant morbi tristique. Vel quam elementum
       pulvinar etiam. Pulvinar pellentesque habitant morbi tristique senectus
     </Collapsible>
+    <Collapsible header="collapse header">
+      Consectetur adipiscing elit pellentesque habitant morbi tristique.
+      Pulvinar pellentesque habitant morbi tristique. Vel quam elementum
+      pulvinar etiam. Pulvinar pellentesque habitant morbi tristique senectus
+    </Collapsible>
+    </div>
   );
-};
-export default App;
+}
 
+export default App;
