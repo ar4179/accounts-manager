b75a44920e16c791d600deaf7955bdd17bf99c20
aryan <ar4179@columbia.edu>
2024-06-24 14:57:05 -0500

    edit and delete icons show/hide based on hover

--- src/components/TaskComp.tsx
+++ src/components/TaskComp.tsx
@@ -1,37 +1,71 @@
 import React from "react";
 import Slider from "@mui/material/Slider";
 import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
 function valuetext(value: number) {
     return `${value}°C`;
 }
 
 const TaskComp: React.FC<{ task: any }> = (props) => {
-    return (
-        <div>
-            {props.task.title}
+    const [isHovered, setIsHovered] = React.useState(false);
 
-            <Box sx={{ width: 300 }}>
-                <Slider
-                    aria-label="Temperature"
-                    defaultValue={props.task.percentComplete}
-                    getAriaValueText={valuetext}
-                    valueLabelDisplay="auto"
-                    color={
-                        props.task.priority === "normal"
-                            ? "primary"
-                            : props.task.priority === "high"
-                            ? "error"
-                            : "success"
-                    }
-                    shiftStep={20}
-                    step={10}
-                    marks
-                    min={0}
-                    max={100}
-                    name={props.task._id}
-                />
-            </Box>
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const buttonStyle = {
+        display: isHovered ? "block" : "none",
+    };
+
+    return (
+        <div
+            className="row"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <div className="col-10">
+                {props.task.title}
+
+                <Box sx={{ width: 400 }}>
+                    <Slider
+                        aria-label="Temperature"
+                        defaultValue={props.task.percentComplete}
+                        getAriaValueText={valuetext}
+                        valueLabelDisplay="auto"
+                        color={
+                            props.task.priority === "normal"
+                                ? "primary"
+                                : props.task.priority === "high"
+                                ? "error"
+                                : "success"
+                        }
+                        shiftStep={20}
+                        step={10}
+                        marks
+                        min={0}
+                        max={100}
+                        name={props.task._id}
+                    />
+                </Box>
+            </div>
+            <div className="col-2">
+                <Stack spacing={1} direction="row" style={buttonStyle}>
+                    <IconButton aria-label="edit" size="small">
+                        <EditOutlinedIcon />
+                    </IconButton>
+                    <IconButton aria-label="delete" size="small">
+                        <DeleteOutlinedIcon />
+                    </IconButton>
+                </Stack>
+            </div>
         </div>
     );
 };
